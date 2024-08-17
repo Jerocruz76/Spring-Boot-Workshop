b03b5d65f34b6f521305cdbbf8fa047621645a67
Jerocruz76 <163927138+Jerocruz76@users.noreply.github.com>
2024-08-16 21:25:39 -0500

    Add files via upload

---
+++ Spring_boot-Workshop/src/test/java/com/riwi/Spring_boot_Workshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.Spring_boot_Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
